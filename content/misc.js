486a7ca1eab03ac4d5f8ef8608ceaa74e048d80c
Arth K. Gajjar <iarthstar@gmail.com>
2023-03-25 12:10:43 +0530

    Added : canonical

--- content/misc.js
+++ content/misc.js
@@ -1,23 +1,23 @@
 export const SERVICES = [
   {
     serviceName: "Brand & Identity",
-    tags: [ "Logo", "Design System", "Print" ],
+    tags: ["Logo", "Design System", "Print"],
   },
   {
     serviceName: "Design",
-    tags: [ "UI / UX", "Research", "Prototype" ],
+    tags: ["UI / UX", "Research", "Prototype"],
   },
   {
     serviceName: "Development",
-    tags: [ "Web", "App", "Backend"],
+    tags: ["Web", "App", "Backend"],
   },
   {
     serviceName: "Digital Marketing",
-    tags: [ "SEO", "SMM", "PPC", "CTR"],
+    tags: ["SEO", "SMM", "PPC", "CTR"],
   },
   {
     serviceName: "Content Creation",
-    tags: [ "Illustration", "Renders", "Copy" ],
+    tags: ["Illustration", "Renders", "Copy"],
   },
 ];
 
@@ -41,36 +41,69 @@ export const SOCIAL_LINKS = [
   { label: "INSTAGRAM", href: "https://instagram.com/honeyhexa" },
 ];
 
+const ORIGIN = "https://www.honeyhexa.com";
+const ROUTES = {
+  ABOUT: '/about',
+  SERVICES: '/services',
+  WORK: '/work',
+  CONTACT: '/contact',
+}
+const addToOrigin = (path) => `${ORIGIN}${path}`;
+
 // ------------------------------------------------------ METADATA ------ START
 
 export const METADATA_HOME = {
-    title: 'Honey Hexa | Home',
-    description: 'We turn your Ideas into Digital Products. Unleash the Power of Your Brand, Improve your online presence and customer experience with us',
-}
+  title: "Honey Hexa | Home",
+  description:
+    "We turn your Ideas into Digital Products. Unleash the Power of Your Brand, Improve your online presence and customer experience with us",
+  alternates: {
+    canonical: ORIGIN,
+  },
+};
 
 export const METADATA_SERVICES = {
-  title: 'Honey Hexa | Services',
-  description: 'We aim to provide you comprehensive suite of services to help your businesses build, maintain, and grow their online presence and brand identity.',
-}
+  title: "Honey Hexa | Services",
+  description:
+    "We aim to provide you comprehensive suite of services to help your businesses build, maintain, and grow their online presence and brand identity.",
+  alternates: {
+    canonical: addToOrigin(ROUTES.SERVICES),
+  },
+};
 
 export const METADATA_ABOUT = {
-  title: 'Honey Hexa | About',
-  description: 'Elevate Your Digital Game with Honey Hexa\'s Cutting-Edge Strategies.',
-}
+  title: "Honey Hexa | About",
+  description:
+    "Elevate Your Digital Game with Honey Hexa's Cutting-Edge Strategies.",
+  alternates: {
+    canonical: addToOrigin(ROUTES.ABOUT),
+  },
+};
 
 export const METADATA_CONTACT = {
-  title: 'Honey Hexa | Contact',
-  description: 'Book Now, Free consultation for your digital needs, we love to connect with our partners and help them with their way forward.',
-}
+  title: "Honey Hexa | Contact",
+  description:
+    "Book Now, Free consultation for your digital needs, we love to connect with our partners and help them with their way forward.",
+  alternates: {
+    canonical: addToOrigin(ROUTES.CONTACT),
+  },
+};
 
 export const METADATA_WORK = {
-  title: 'Honey Hexa | Work',
-  description: 'Design portfolio and case studies of branding, marketing website, UI/UX design, and mobile app design work by Honey Hexa.',
-}
+  title: "Honey Hexa | Work",
+  description:
+    "Design portfolio and case studies of branding, marketing website, UI/UX design, and mobile app design work by Honey Hexa.",
+  alternates: {
+    canonical: addToOrigin(ROUTES.WORK),
+  },
+};
 
 export const METADATA_NOT_FOUND = {
-  title: 'Honey Hexa | 404',
-  description: 'Book Now, Free consultation for your digital needs, we love to connect with our partners and help them with their way forward.',
-}
+  title: "Honey Hexa | 404",
+  description:
+    "Book Now, Free consultation for your digital needs, we love to connect with our partners and help them with their way forward.",
+  alternates: {
+    canonical: ORIGIN,
+  },
+};
 
 // ------------------------------------------------------ METADATA -------- END
