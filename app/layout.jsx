38a2eaddaf2074f5aadc09bcc7681b749a0fbaee
Arth K. Gajjar <iarthstar@gmail.com>
2023-10-07 17:27:17 +0530

    Added : GTM Code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import Script from "next/script";
+import "./globals.css";
 
 export default function RootLayout({
   // Layouts must accept a children prop.
@@ -7,7 +8,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <Script id="google-tag-manager">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PZN7WMHL');
+          `}
+        </Script>
+      </head>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PZN7WMHL"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }
