a7dc7adfbaed192cb3bd54e0c17ae62681b7b649
Arth K. Gajjar <iarthstar@gmail.com>
2024-05-22 02:11:29 +0530

    added : GoogleTagManager

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 export default function RootLayout({
@@ -11,30 +11,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://tally.so" />
-        {process.env.VERCEL_ENV === "production" ? (
-          <Script id="google-tag-manager">
-            {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PZN7WMHL');
-          `}
-          </Script>
-        ) : null}
       </head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PZN7WMHL"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
         {children}
         <Analytics />
+        {process.env.VERCEL_ENV === "production" ? (
+          <GoogleTagManager gtmId="GTM-PZN7WMHL" />
+        ) : null}
       </body>
     </html>
   );
